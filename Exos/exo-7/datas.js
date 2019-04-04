c15606490b0772ec02d0b08a72735c1c03cbb526
zan <clement.mortas@le-campus-numerique.fr>
2019-04-04 14:16:03 +0200

    fin exo 7

--- Exos/exo-7/datas.js
+++ Exos/exo-7/datas.js
@@ -1,58 +1,116 @@
-var jsonDatas=[
-	{
-		"name" :"Fiat Punto",
-		"type" : "car",
-		"description" : "Je suis une voiture",
-		"price" : 10000,
-		"quantity" : 2
-	},{
-		"name" :"Porsche 911",
-		"type" : "car",
-		"description" : "Je suis une belle voiture",
-		"price" : 80000,
-		"quantity" : 0
-	},{
-		"name" :"Villa sur la plage",
-		"type" : "hou" +
-			"se",
-		"description" : "Quelle belle vue",
-		"price" : 870000,
-		"quantity" : 1
-	},{
-		"name" :"Peugeot 205",
-		"type" : "car",
-		"description" : "Je suis une autre voiture",
-		"price" : 2000,
-		"quantity" : 2
-	},{
-		"name" :"Maison à la campagne",
-		"type" : "house",
-		"description" : "Vive le calme",
-		"price" : 170000,
-		"quantity" : 3
-	},{
-		"name" :"Monopoly",
-		"type" : "game",
-		"description" : "",
-		"price" : 30,
-		"quantity" : 300
-	},{
-		"name" :"Mario Bros",
-		"type" : "videoGame",
-		"description" : "",
-		"price" : 30,
-		"quantity" : 200
-	},{
-		"name" :"Place VIP concert Metallica",
-		"type" : "show",
-		"description" : "",
-		"price" : 800,
-		"quantity" : 10
-	},{
-		"name" :"Entrée au parc Astérix",
-		"type" : "show",
-		"description" : "",
-		"price" : 30,
-		"quantity" : 200
-	}
-]
+var jsonDatas = [
+    {
+        "type": "car",
+        "items": [
+            {
+                "name": "Fiat Punto",
+                "description": "Je suis une voiture",
+                "price": 10000,
+                "quantity": 2,
+                "contact": {
+                    "lastName": "Dubois",
+                    "firstName": "Martin",
+                    "address": "1 Grande Rue 74000 Annecy"
+                }
+            }, {
+                "name": "Porsche 911",
+                "description": "Je suis une belle voiture",
+                "price": 80000,
+                "quantity": 0,
+                "contact": {
+                    "lastName": "Mortas",
+                    "firstName": "Clément",
+                    "address": "19 allée des Ecombes 74320 Sevrier"
+                }
+            }, {
+                "name": "Peugeot 205",
+                "description": "Je suis une autre voiture",
+                "price": 2000,
+                "quantity": 2,
+                "contact": {
+                    "lastName": "Pablo",
+                    "firstName": "Callejo",
+                    "address": "27 rue Royal 74590 Faverge"
+                }
+            }]
+    }, {
+        "type": "house",
+        "items": [
+            {
+                "name": "Villa sur la plage",
+                "description": "Quelle belle vue",
+                "price": 870000,
+                "quantity": 1,
+                "contact": {
+                    "lastName": "Han",
+                    "firstName": "Solo",
+                    "address": "1 rue du tavail 13000 Marseille"
+                }
+            }, {
+                "name": "Maison à la campagne",
+                "description": "Vive le calme",
+                "price": 170000,
+                "quantity": 3,
+                "contact": {
+                    "lastName": "Iron",
+                    "firstName": "Man",
+                    "address": "1 rue de la Monnaie 85000 Roubaix"
+                }
+            }]
+    }, {
+        "type": "game",
+        "items": [
+            {
+                "name": "Monopoly",
+                "description": "",
+                "price": 30,
+                "quantity": 300,
+                "contact": {
+                    "lastName": "Iron",
+                    "firstName": "Man",
+                    "address": "1 rue de la Monnaie 85000 Roubaix"
+                }
+            }]
+    }, {
+        "type": "videoGame",
+        "items": [
+            {
+                "name": "Mario Bros",
+                "description": "",
+                "price": 30,
+                "quantity": 200,
+                "contact": {
+                    "lastName": "Luigi",
+                    "firstName": "Wrath",
+                    "address": "1 rue du 2 74000 Annecy"
+                }
+            }]
+
+
+    }, {
+        "type": "show",
+        "items": [
+            {
+                "name": "Place VIP concert Metallica",
+                "description": "",
+                "price": 800,
+                "quantity": 10,
+                "contact": {
+                    "lastName": "Bob",
+                    "firstName": "Margera",
+                    "address": "18 rue Normal 85450 Nouille-Iorque"
+                }
+            }, {
+                "name": "Entrée au parc Astérix",
+                "type": "show",
+                "description": "",
+                "price": 30,
+                "quantity": 200,
+                "contact": {
+                    "lastName": "Arya",
+                    "firstName": "Starck",
+                    "address": "14 avenue du décès 76830 Lausse-Enjelaise"
+                }
+            }]
+    }
+];
--- Exos/exo-7/exo7.js
+++ Exos/exo-7/exo7.js
@@ -8,103 +8,125 @@ var type = {
     'show': 'spectacle'
 }
 console.log(type);
-console.log(jsonDatas);
 
 function trad() {
-        for (let i = 0; i < jsonDatas.length; i++){
-            for (var property in type){
-                if (property === jsonDatas[i].type){
-                    jsonDatas[i].type = type[property];
-                }
+    for (let i = 0; i < jsonDatas.length; i++) {
+        for (var property in type) {
+            if (property === jsonDatas[i].type) {
+                jsonDatas[i].type = type[property];
             }
         }
+    }
 
     console.log(jsonDatas);
 }
 
-function search(){
+function search() {
     console.clear();
     var check = document.getElementById('myCheck');
     var message = document.test.message.value;
     var response = [];
-    for (let i = 0; i < jsonDatas.length; i++) {
-        if (message === jsonDatas[i].type && check.checked === true){
-            if (jsonDatas[i].quantity > 0){
-                response.push(jsonDatas[i]);
-            }
-        }else if(message === jsonDatas[i].type){
-            response.push(jsonDatas[i]);
-        }
+    jsonDatas.forEach(function (type) {
 
+            if (type.type === message && check.checked === true) {
+                type.items.forEach(function (item) {
+                    if (item.quantity > 0) {
+                        response.push(item);
+                        console.log(sortItems(response));
+                    }
+                });
+            } else if (type.type === message) {
+                type.items.forEach(function (item) {
+                    response.push(item);
+
+                });
+            } else if(message === '' && check.checked === true){
+                type.items.forEach(function (item) {
+                    if (item.quantity > 0) {
+                        response.push(item);
+                        console.log(sortItems(response));
+                    }
+                });
+            }else if (message === ''){
+                type.items.forEach(function (item) {
+                    response.push(item);
+
+                });
+            }
+        }
+    );
+    sortItems(response);
+}
+
+function sortItems(response) {
+    console.clear()
+
+    var sort = document.getElementById("sort")
+    var choiceUser = sort.options[sort.selectedIndex].value;
+
+    if (choiceUser === '0') {
+
+    } else if (choiceUser === '1') {
+        response.sort(function (a, b) {//dans response je trie avec la fonction sort()
+            return (a.name).localeCompare(b.name);//je retourne le resultat du tri par (name) a > b donc croissant
+        });
+    } else if (choiceUser === '2') {
+        response = response.sort(function (a, b) {
+            return (b.name).localeCompare(a.name);
+        });
+    } else if (choiceUser === '3') {
+        response.sort(function (a, b) {
+            return parseFloat(a.price) - parseFloat(b.price);
+        });
+    } else if (choiceUser === '4') {
+        response.sort(function (a, b) {
+            return parseFloat(b.price) - parseFloat(a.price);
+        });
     }
-        document.getElementById('message').innerHTML = message;
     console.log(response);
 }
 
-function sortItems() {
-    console.clear();
-    var sort = document.getElementById("sort")
-    var choiceUser = sort.options[sort.selectedIndex].value;
-       for (let i = 0; i < jsonDatas.length; i++) {
-        if (choiceUser === '1'){
-           jsonDatas.sort(function (a, b) {
-                var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
-                if (nameA < nameB) //sort string ascending
-                    return -1
-                if (nameA > nameB)
-                    return 1
-                return 0
-            });
-        }else  if (choiceUser === '2'){
-            jsonDatas.sort(function (a, b) {
-                var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase()
-                if (nameA > nameB) //sort string ascending
-                    return -1
-                if (nameA < nameB)
-                    return 1
-                return 0
-            });
-        }else if (choiceUser === '3'){
-               jsonDatas.sort(function (a, b) {
-                   var nameA=a.price, nameB=b.price
-                   if (nameA < nameB) //sort string ascending
-                       return -1
-                   if (nameA > nameB)
-                       return 1
-                   return 0
-               });
-           } else if (choiceUser === '4'){
-               jsonDatas.sort(function (a, b) {
-                   var nameA=a.price, nameB=b.price
-                   if (nameA > nameB) //sort string ascending
-                       return -1
-                   if (nameA < nameB)
-                       return 1
-                   return 0
-               });
-           }
-    }
-    console.log(jsonDatas);
-}
 
 function addItem() {
-
+    console.clear();
     var name = document.Item.name.value;
     var type = document.Item.type.value;
     var description = document.Item.description.value;
     var price = document.Item.price.value;
     var quantity = document.Item.quantity.value;
+    var firstname = document.Item.firstname.value
+    var lastname = document.Item.lastname.value
+    var address = document.Item.address.value
     var newItem = {};
-
     newItem.name = name;
-    newItem.type = type;
     newItem.description = description;
     newItem.price = price;
     newItem.quantity = quantity;
-    jsonDatas.push(newItem);
+    newItem.contact = {firstname, lastname, address};
 
-    console.log(newItem);
+    jsonDatas.forEach(function (add) {
+        if (add.type === type) {
+            add.items.push(newItem);
+        } else {
+            return false
+        }
+    });
+
+
     console.log(jsonDatas);
+    // console.log(jsonDatas);
 
 }
+
+function showContact() {
+    jsonDatas.forEach(function (type) {
+            type.items.forEach(function (item) {
+                console.log(item.contact);
+            })
+        }
+    );
+}
+
+
 trad();
+// showContact();
